cc8b0fe33bad4ebc0363892d4e627497315877dd
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-10-10 10:51:26 -0400

    loops practice

---
+++ index.js
@@ -0,0 +1,23 @@
+// say hello three times//
+ for(let i = 0;i < 3 ; i++){
+ console.log('hello')
+}
+
+
+
+/* 
+0
+1
+2
+3
+*/
+
+for(let i  = 0; i < 4; i++){
+    console.log((i))
+}
+const people = ['todd ', 'andrew ', 'derek ']
+for(let i =  0; i < people.length; i ++){
+console.log('hello ' + people[i])
+}
+//make a loop that says hello to each person in the array 
+
---
+++ loopsChallenge.js
@@ -0,0 +1,6 @@
+const videos = ['cats', 'news', 'games', 'sports']
+//cosole log eacah of these vidoes to the screen 
+// make sure it works for any length of array 
+for (let i = 0 ; i < videos.length ; i++){
+console.log(videos[i])
+}
